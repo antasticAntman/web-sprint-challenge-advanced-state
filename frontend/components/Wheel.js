30449cf338eaf087a015a8c19f90d903092dda63
Anthony Climer <anthonyclimer0925@gmail.com>
2023-03-09 17:22:05 -0500

    The wheel works today :)

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,20 +1,65 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import { moveClockwise, moveCounterClockwise } from '../state/action-creators'
 
-export default function Wheel(props) {
+function Wheel(props) {
+    // console.log('wheel State:', state.number)
+    const {number, moveClockwise, moveCounterClockwise} = props
+    console.log('wheel state:' , number)
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div className=
+          {number !== 0 ? 'cog' : 'cog active'} 
+          style={{ "--i": 0 }}>
+          {number !== 0 ? '' : 'B'}
+        </div>
+
+        <div className=
+          {number !== 1 ? 'cog' : 'cog active'} 
+          style={{ "--i": 1 }}>
+          {number !== 1 ? '' : 'B'}  
+        </div>
+
+        <div className=
+          {number !== 2 ? 'cog' : 'cog active'} 
+          style={{ "--i": 2 }}>
+          {number !== 2 ? '' : 'B'}
+        </div>
+
+        <div className=
+          {number !== 3 ? 'cog' : 'cog active'} 
+          style={{ "--i": 3 }}>
+          {number !== 3 ? '' : 'B'}
+        </div>
+
+        <div className=
+          {number !== 4 ? 'cog' : 'cog active'} 
+          style={{ "--i": 4 }}>
+          {number !== 4 ? '' : 'B'}
+        </div>
+
+        <div className=
+          {number !== 5 ? 'cog' : 'cog active'}
+          style={{ "--i": 5 }}>
+          {number !== 5 ? '' : 'B'}
+        </div>
+        
+        {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={()=>moveCounterClockwise()} >Counter clockwise</button>
+        <button id="clockwiseBtn" onClick={()=>moveClockwise()}>Clockwise</button>
       </div>
     </div>
   )
 }
+
+const mapStateToProps = state => {
+  return {
+    number: state.wheel.number,
+    // wheel: state.wheel
+  }
+}
+
+export default connect(mapStateToProps, {moveClockwise, moveCounterClockwise})(Wheel);
--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,11 +1,41 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
+import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE } from './action-types'
 
-const initialWheelState = 0
-function wheel(state = initialWheelState, action) {
-  return state
+const initialWheelState = {
+ number: 0,
 }
-
+ function wheel(state = initialWheelState, action) {
+  switch(action.type){
+    case MOVE_CLOCKWISE:
+      if(state.number === 5) {
+        return {
+          ...state,
+          number:0
+        }
+      }
+    console.log('MOVE_CLOCWISE:',state)
+      return {
+        ...state,
+        number: state.number + 1,
+      }
+    case MOVE_COUNTERCLOCKWISE:
+      if(state.number ===0){
+        console.log('state',state.number)
+        return {
+          ...state,
+          number: 5
+        } 
+      }
+      return {
+        ...state,
+        number: state.number - 1,
+      }
+    default:
+      console.log('default state:',state)
+    return state
+}
+}
 const initialQuizState = null
 function quiz(state = initialQuizState, action) {
   return state
